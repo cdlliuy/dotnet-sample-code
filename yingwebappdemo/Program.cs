c5e2d5dd94336dfec4fc35a56f28d7b154fa039d
Ying Liu <lying@microsoft.com>
2022-03-29 17:58:34 +0800

    ying web app demo

---
+++ yingwebappdemo/Program.cs
@@ -0,0 +1,82 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace yingwebappdemo
+{
+    public class Program
+    {
+
+        public static ServiceCancellationTokenSource cancellationTokenSource = new ServiceCancellationTokenSource(900, 60, 5);
+
+        private static void ProcessExit(object? sender, EventArgs eventArgs)
+        {
+            cancellationTokenSource.Cancel();
+            shutdown();
+        }
+
+        public static async Task Main(string[] args)
+        {
+
+            try
+            {
+                AppDomain.CurrentDomain.ProcessExit += ProcessExit;
+
+                CreateHostBuilder(args).Build().RunAsync();
+                await pollingTasks(cancellationTokenSource);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Thrown exception: {e}");
+            }
+            finally
+            {
+                Console.WriteLine($"{DateTime.UtcNow} : Main: Executing of finally");
+            }
+
+
+        }
+
+        public static void shutdown()
+        {
+            Console.WriteLine($"{DateTime.UtcNow} : Main-Shutdown: Handling the in-progressing tasks");
+            if (cancellationTokenSource.IsCancellationRequested)
+            {
+                cancellationTokenSource.WaitAndExit();
+            }
+            Console.WriteLine($"{DateTime.UtcNow} : Main-Shutdown: Completely shutdown");
+        }
+
+
+        public async static Task pollingTasks(ServiceCancellationTokenSource cancellationTokenSource)
+        {
+
+            int count = 0;
+            while (!cancellationTokenSource.IsCancellationRequested)
+            {
+                Console.WriteLine($"{DateTime.UtcNow}: pollingTasks: task {count} started");
+                cancellationTokenSource.IncrementExecutionCount();
+                await Task.Delay(10 * 1000); //10s
+                cancellationTokenSource.DecrementExecutionCount();
+                Console.WriteLine($"{DateTime.UtcNow}: pollingTasks: task {count} ended");
+                count++;
+            }
+            Console.WriteLine($"{DateTime.UtcNow}: pollingTasks: stop task polling when the process is cancelled.");
+            
+
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+
+    }
+}
---
+++ yingwebappdemo/ServiceCancellationTokenSource.cs
@@ -0,0 +1,59 @@
+﻿namespace yingwebappdemo
+{
+    using System.Threading;
+    using System;
+    public class ServiceCancellationTokenSource : CancellationTokenSource
+    {
+        private int executionCount;
+        private bool cancelTask;
+        private int remainingWaitTime;      // time in seconds
+        private readonly int cancelTime;              // time in seconds
+        private readonly int sleepTime;      // time in seconds
+
+        public ServiceCancellationTokenSource(int waitTime, int cancelTime, int sleepTime)
+        {
+            this.executionCount = 0;
+            this.remainingWaitTime = waitTime;
+            this.cancelTime = cancelTime;
+            this.sleepTime = sleepTime;
+        }
+
+        public void IncrementExecutionCount()
+        {
+            Interlocked.Increment(ref executionCount);
+        }
+        public void DecrementExecutionCount()
+        {
+            Interlocked.Decrement(ref executionCount);
+        }
+
+        public int ExecutionCount => executionCount;
+
+        public bool CancleTask()
+        {
+            return cancelTask;
+        }
+
+        public void WaitAndExit(int minWaitTimeInSeconds = 1)
+        {
+            Console.WriteLine($"The number of pending task: {executionCount}");
+
+            // Sleep miniWaitTimeInSeconds secs before checking the execution count.
+            Thread.Sleep(minWaitTimeInSeconds * 1000);
+
+            // wait to allow some on going task to finish
+            // set canceltask flag when there it below the threshold
+            while (remainingWaitTime > 0 && executionCount > 0)
+            {
+                Console.WriteLine($"WaitAndExit - remainingWaitTime: {remainingWaitTime}, cancelTime: {cancelTime}, sleepTime: {sleepTime}, executionCount: {executionCount}");
+
+                remainingWaitTime -= sleepTime;
+                if (remainingWaitTime < cancelTime)
+                {
+                    cancelTask = true;
+                }
+                Thread.Sleep(sleepTime * 1000);
+            }
+        }
+    }
+}
