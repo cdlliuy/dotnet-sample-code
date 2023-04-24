7cbfb9b380ca8859ed11547727c294098f4c3174
Ying Liu <lying@microsoft.com>
2023-04-24 22:47:32 +0800

    using net6 webapplication

--- yingwebappdemo/Program.cs
+++ yingwebappdemo/Program.cs
@@ -1,10 +1,13 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace yingwebappdemo
@@ -14,30 +17,39 @@ namespace yingwebappdemo
 
         public static ServiceCancellationTokenSource cancellationTokenSource = new ServiceCancellationTokenSource(900, 60, 5);
 
-        private static void ProcessExit(object? sender, EventArgs eventArgs)
-        {
-            Console.WriteLine($"Process exit triggered");
-            cancellationTokenSource.Cancel();
-            shutdown();
-        }
-
-        private static void ControlCHandler(object? sender, EventArgs eventArgs)
-        {
-            Console.WriteLine($"Ctrl+C triggered");
-            cancellationTokenSource.Cancel();
-            shutdown();
-        }
 
         public static async Task Main(string[] args)
         {
-
             try
             {
-                AppDomain.CurrentDomain.ProcessExit += ProcessExit;
-                //Console.CancelKeyPress += ControlCHandler;
+                WebApplicationBuilder appBuilder = WebApplication.CreateBuilder(args);
+                appBuilder.Host.ConfigureHostOptions(o => o.ShutdownTimeout = TimeSpan.FromSeconds(60)); //valid in .net6
+                appBuilder.Services.AddControllers();
 
-                CreateHostBuilder(args).Build().RunAsync();
-                //System.Threading.Thread.Sleep(300 * 1000);
+                WebApplication app = appBuilder.Build();
+
+                var lifetime = app.Services.GetService<IHostApplicationLifetime>();
+
+                lifetime.ApplicationStarted.Register(() =>
+                {
+                    Console.WriteLine($"{DateTime.UtcNow} Application started");
+                });
+
+                lifetime.ApplicationStopping.Register(() =>
+                {
+                    Console.WriteLine($"{DateTime.UtcNow} Application stopping");
+                    //Thread.Sleep(120 * 1000);
+                    cancellationTokenSource.Cancel();
+                });
+
+                lifetime.ApplicationStopped.Register(() =>
+                {
+                    Console.WriteLine($"{DateTime.UtcNow} Application stopped");
+                });
+                app.UseRouting();
+                app.MapControllers();
+                app.RunAsync();
+
                 await pollingTasks(cancellationTokenSource);
             }
             catch (Exception e)
@@ -74,17 +86,7 @@ namespace yingwebappdemo
                 count++;
             }
             Console.WriteLine($"{DateTime.UtcNow}: pollingTasks: stop task polling when the process is cancelled.");
-            
-
         }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureHostOptions(o => o.ShutdownTimeout = TimeSpan.FromSeconds(60)) //valid in .net6
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
 
     }
 }
--- yingwebappdemo/build-deploy.sh
+++ yingwebappdemo/build-deploy.sh
@@ -7,9 +7,8 @@ rm -rf ./build/*
 dotnet build "yingwebappdemo.csproj" -c Release -o ./build
 docker build -t cdlliuy/yingtestappwindows:$tag -f ./Dockerfile.windows .
 #docker push cdlliuy/yingtestappwindows:$tag
-popd
 
-#kubectl delete -f sample-deployment-raw.yaml
-#kubectl apply -f sample-deployment-raw.yaml
-kubectl set image deployment/yingtestapp-raw-deployment yingtestapp-raw=cdlliuy/yingtestappwindows:$tag
+kubectl delete -f sample-deployment-raw.yaml
+kubectl apply -f sample-deployment-raw.yaml
+#kubectl set image deployment/yingtestapp-raw-deployment yingtestapp-raw=cdlliuy/yingtestappwindows:$tag
 kubectl get pods -w --request-timeout='10s'
